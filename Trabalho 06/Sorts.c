fcb207703afec2b98ecc5f66ccd30885b888110c
luisfelipeandrade19 <luisfelipe7014feups@gmail.com>
2024-09-05 11:13:07 -0300

    add selectionSort

--- Trabalho 06/Sorts.c
+++ Trabalho 06/Sorts.c
@@ -10,9 +10,26 @@ void bubbleSort(int *v, int n){
     }
 }
 
-
 void swap(int *a, int* b){
     int temp = *a;
     *a = *b;
     *b = temp;
+}
+
+
+void selectionSort(int *num, int tam) {
+ int i, j, min, aux;
+
+ for (i = 0; i < (tam-1); i++) {
+ min = i;
+
+ for (j = (i+1); j < tam; j++) {
+ if(num[j] < num[min])
+ min = j;
+ } 
+
+ if (i != min) 
+ swap(&num[i], &num[min]);
+
+ }
 }
