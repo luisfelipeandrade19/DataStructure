4fb664e8ff0322158a82291648e1d38d562b1653
luisfelipeandrade19 <luisfelipe7014feups@gmail.com>
2024-09-05 13:54:44 -0300

    algumas mudancas

---
+++ Trabalho 06/BubbleSort/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "Sorts.c"
+
+int gerador(){
+ srand(time(NULL));
+ return rand() % 1000;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    double tempo;
+    clock_t inicio, fim;
+
+    inicio = clock();
+    fim = clock();
+
+    tempo = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+    printf("\nTempo gasto: %g s.\n", tempo);
+
+    return 0;
+}
